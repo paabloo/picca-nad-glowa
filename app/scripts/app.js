cb2f550cdcf64bdad0f286ba2be5da21c0fab364
Pawel Kromplewski <pawel.kromplewski@pagepro.pl>
2015-07-31 11:04:45 +0200

    remove some files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,26 +1,4 @@
 'use strict';
 
-var piccaApp = angular.module('picca.app', [
-  'ngRoute'
-  ])
-.config([
-  '$routeProvider',
-  function($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'partials/homePageTpl.html',
-        controller: 'HomeController',
-        controllerAs: 'HC'
-      })
-      .when('/licz_popek', {
-        templateUrl: 'partials/popekPageTpl.html',
-        controller: 'PopekController',
-        controllerAs: 'PC'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-}])
-.run([function () {
-  console.log('run');
-}]);
+ // module, include router, config, run
+
--- app/scripts/services/pizzaSrv.js
+++ app/scripts/services/pizzaSrv.js
@@ -1,12 +1,1 @@
 'use strict';
-
-piccaApp.service('PizzaService', [
-  'PizzaFactory',
-  function (PizzaFactory) {
-
-    var self = this;
-
-    self.getPizzas = function PS_getPizzas () {
-      return PizzaFactory.getPizzas();
-    }
-}]);
